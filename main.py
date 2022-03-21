882a3dbfe8d271dd758be206e074a3f59953adb6
xLr864 <2018pietcschetan42@poornima.org>
2022-03-21 11:58:05 +0530

    Added FastAPI app

---
+++ main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+import random
+app = FastAPI()
+
+Recommanded_Games = ["GTA 5","Red Dead Redemption 2","Assassins Creed Valhalla","Valorant","CS:GO","Sleeping Dogs","Need for Speed","Resident Evil"]
+@app.get("/")
+async def root():
+    choice = random.randint(0,len(Recommanded_Games))
+    return {"message":Recommanded_Games[choice]}
